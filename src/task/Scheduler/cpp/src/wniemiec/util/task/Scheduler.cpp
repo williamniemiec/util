4868093b04b216eb3534d2bc946908f3fc1a09b0
William Niemiec <williamniemiec@hotmail.com>
2021-05-23 00:33:00 -0300

    Refactoring

--- src/task/Scheduler/cpp/src/wniemiec/util/task/Scheduler.cpp
+++ src/task/Scheduler/cpp/src/wniemiec/util/task/Scheduler.cpp
@@ -11,12 +11,12 @@ std::function<void(void)> null_routine = [](){};
 //-------------------------------------------------------------------------
 //		Attributes
 //-------------------------------------------------------------------------
-std::map<unsigned long, bool> Scheduler::delayRoutines = std::map<unsigned long, bool>();
-std::map<unsigned long, bool> Scheduler::intervalRoutines = std::map<unsigned long, bool>();
-std::map<unsigned long, bool> Scheduler::timeoutRoutine = std::map<unsigned lonf, bool>();
-std::function<void(void)>& Scheduler::currentRoutine = null_routine;
-unsigned long Scheduler::currentRoutineId;
-pthread_t Scheduler::controlThread;
+std::map<unsigned long, bool> Scheduler::delay_routines = std::map<unsigned long, bool>();
+std::map<unsigned long, bool> Scheduler::interval_routines = std::map<unsigned long, bool>();
+std::map<unsigned long, bool> Scheduler::timeout_routine = std::map<unsigned long, bool>();
+std::function<void(void)>& Scheduler::current_routine = null_routine;
+unsigned long Scheduler::current_routine_id;
+pthread_t Scheduler::control_thread;
 
 
 //-------------------------------------------------------------------------
@@ -25,78 +25,78 @@ pthread_t Scheduler::controlThread;
 unsigned long Scheduler::set_timeout(const std::function<void(void)>& routine, long delay)
 {
     initialize_routine_id();
-    currentRoutine = routine;
+    current_routine = routine;
 
     pthread_t thread;
     pthread_create(&thread, nullptr, delay_control_routine, (void*) delay);
 
-    return currentRoutineId;
+    return current_routine_id;
 }
 
 void Scheduler::initialize_routine_id()
 {
     std::this_thread::sleep_for(std::chrono::seconds(1));
-    currentRoutineId = get_current_time();
+    current_routine_id = get_current_time();
 }
 
 void* Scheduler::delay_control_routine(void* arg)
 {
     long delay = (long) arg;
-    unsigned long id = currentRoutineId;
-    const std::function<void(void)> routine = currentRoutine;
-    
+    unsigned long id = current_routine_id;
+    const std::function<void(void)> routine = current_routine;
+
     if (delay < 0)
         throw std::invalid_argument("Delay cannot be negative");
 
-    delayRoutines[id] = true;
+    delay_routines[id] = true;
 
     std::this_thread::sleep_for(std::chrono::milliseconds(delay));
 
-    if (delayRoutines[id])
+    if (delay_routines[id])
         routine();
-    
+
     return nullptr;
 }
 
-void Scheduler::clear_timeout(unsignedlong id)
+void Scheduler::clear_timeout(unsigned long id)
 {
-    delayRoutines[id] = false;
+    delay_routines[id] = false;
 }
 
 unsigned long Scheduler::set_interval(const std::function<void(void)>& routine, long interval)
 {
     initialize_routine_id();
-    currentRoutine = routine;
+    current_routine = routine;
 
     pthread_t thread;
     pthread_create(&thread, nullptr, interval_control_routine, (void*) interval);
 
-    return currentRoutineId;
+    return current_routine_id;
 }
 
 void* Scheduler::interval_control_routine(void* arg)
 {
     long interval = (long) arg;
-    unsigned long id = currentRoutineId;
-    const std::function<void(void)> routine = currentRoutine;
+    unsigned long id = current_routine_id;
+    const std::function<void(void)> routine = current_routine;
 
     if (interval < 0)
         throw std::invalid_argument("Interval cannot be negative");
-    
-    intervalRoutines[id] = true;
 
-    while (intervalRoutines[id])
+    interval_routines[id] = true;
+
+    while (interval_routines[id])
     {
         std::this_thread::sleep_for(std::chrono::milliseconds(interval));
         routine();
     }
-    
+
     return nullptr;
 }
 
 void Scheduler::clear_interval(unsigned long id)
 {
-    intervalRoutines[id] = false;
+    interval_routines[id] = false;
 }
 
 bool Scheduler::set_timeout_to_routine(const std::function<void(void)>& routine, long timeout)
@@ -112,10 +112,10 @@ void Scheduler::run_routine(const std::function<void(void)>& routine)
 {
     initialize_routine_id();
 
-    timeoutRoutine[currentRoutineId] = false;
-    currentRoutine = routine;
+    timeout_routine[current_routine_id] = false;
+    current_routine = routine;
 
-    pthread_create(&controlThread, nullptr, control_routine, nullptr);
+    pthread_create(&control_thread, nullptr, control_routine, nullptr);
 }
 
 time_t Scheduler::get_current_time()
@@ -125,12 +125,12 @@ time_t Scheduler::get_current_time()
 
 void* Scheduler::control_routine(void* args)
 {
-    time_t id = currentRoutineId;
-    std::function<void(void)>& routine = currentRoutine;
+    time_t id = current_routine_id;
+    std::function<void(void)>& routine = current_routine;
 
     routine();
-    timeoutRoutine[id] = true;
-    
+    timeout_routine[id] = true;
+
     return nullptr;
 }
 
@@ -151,10 +151,10 @@ double Scheduler::time_elapsed_in_milliseconds(time_t start)
 
 bool Scheduler::has_routine_finished()
 {
-    return timeoutRoutine[currentRoutineId];
+    return timeout_routine[current_routine_id];
 }
 
 void Scheduler::finish_routine()
 {
-    pthread_cancel(controlThread);
+    pthread_cancel(control_thread);
 }
